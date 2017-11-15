f6ad8bd8b18f5bea711e8d8e6200ec3696a4abd5
Matt Hensley <matt.hensley777@gmail.com>
2017-11-14 20:33:44 -0600

    done

--- javascripts/controllers/searchCtrl.js
+++ javascripts/controllers/searchCtrl.js
@@ -2,10 +2,11 @@
 
 
 app.controller("SearchCtrl", function($scope, tmdbService) {
+	$scope.movies = [];
             $scope.enterPush = (event) => {
                 if (event.keyCode === 13) {
                     tmdbService.searchMovies(event.target.value).then((results) => {
-                        console.log("movies?", results.data.results);
+                      $scope.movies = results.data.results;
                     }).catch((err) => {
                         console.log("error in searchMovies", err);
                     });
